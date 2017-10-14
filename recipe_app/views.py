a5fc66a0e94914a058d71ee5b02a8ebda8a522d8
Dimitar Bezinski <d.bezinski@gmail.com>
2017-10-14 12:46:55 +0100

    Add Stripe Payment

--- recipe_app/views.py
+++ recipe_app/views.py
@@ -1,6 +1,8 @@
 from django.views import generic
 from django.views.generic.edit import CreateView
 from .models import Recipe
+from django.http import request
+
 
 from django.shortcuts import render
 
@@ -8,8 +10,17 @@ class IndexView(generic.ListView):
     template_name = 'recipe_app/index.html'
     context_object_name = "all_recipes"
 
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context.update({
+            'all_genres': Recipe.objects.all(),
+        })
+        return context
+
     def get_queryset(self):
-        return Recipe.objects.all()
+        query = self.request.GET.get('q')
+        if query:
+            return Recipe.objects.filter(recipe_name__icontains=query)
 
 class DetailView(generic.DetailView):
     model = Recipe
@@ -21,3 +32,6 @@ class RecipeCreate(CreateView):
 
 def get_index(request):
     return render(request, 'recipe_app/index.html')
+
+
+
