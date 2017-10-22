958302ed88d948ccb8269c7e3974d7d875cc3bd9
Dimitar Bezinski <d.bezinski@gmail.com>
2017-10-22 19:30:32 +0100

    New File

--- recipe_prj/wsgi.py
+++ recipe_prj/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for recipe_prj project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recipe_prj.settings")
 
 application = get_wsgi_application()
