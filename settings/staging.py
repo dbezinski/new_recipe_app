0363fddf55709a90619f9679b5a2478c22640746
Dimitar Bezinski <d.bezinski@gmail.com>
2017-10-23 19:29:30 +0100

    test deyploy

--- settings/staging.py
+++ settings/staging.py
@@ -4,13 +4,8 @@ import dj_database_url
 DEBUG = False
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
 }
-
-DATABASES['default'] = dj_database_url.config("CLEARDB_DATABASE_URL")
 
 # Stripe environment variables
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_lRJ210v4A3Z1wdwpi5469nZj')
