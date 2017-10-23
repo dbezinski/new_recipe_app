b33a3b0a5c7bb583b69a848c7288ffeaac54528c
Dimitar Bezinski <d.bezinski@gmail.com>
2017-10-23 21:09:02 +0100

    test deyploy

--- recipe_prj/wsgi.py
+++ recipe_prj/wsgi.py
@@ -1,8 +1,9 @@
 import os
+
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recipe_prj.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.staging")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
