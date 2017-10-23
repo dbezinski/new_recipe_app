df91bfe02cf269ec9b4d03446989927a029a7c03
Dimitar Bezinski <d.bezinski@gmail.com>
2017-10-23 20:57:06 +0100

    test deyploy

--- settings/dev.py
+++ settings/dev.py
@@ -13,5 +13,5 @@ DATABASES = {
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_lRJ210v4A3Z1wdwpi5469nZj')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET', 'sk_test_WQE2qg0XNQ0kqIOw8n0Ht0zu')
 
-STATIC_URL = 'http://127.0.0.1:8000/recipe_app'
+STATIC_URL = 'http://127.0.0.1:8000/recipe_app/'
 SITE_URL = ['http://127.0.0.1:8000', 'http://food-recipe-app.herokuapp.com/']
