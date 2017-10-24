27f3ff4ff03d404ac815e1a17f8c3cfcecfb3317
Dimitar Bezinski <d.bezinski@gmail.com>
2017-10-24 21:45:27 +0100

    Test Deploy

--- recipe_prj/urls.py
+++ recipe_prj/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     url(r'payment-accepted/$', views.payment_accepted, name='payment-accepted'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
